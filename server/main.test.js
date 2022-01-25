a60b66a2c4157273a951212ac2d28fbfb0a17de7
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2022-01-24 21:28:18 -0700

    Added e2e testing using puppeteer
    - added end-to-end test for a repo that exists with no open pull requests
    - added e2e test for repo that doesn't exist -> 404
    - added e2e test for repo with open pull requests

--- server/main.test.js
+++ server/main.test.js
@@ -1,4 +1,5 @@
-const router = require('./routes/index');
+const router = require('./routes/index'),
+puppeteer = require('puppeteer');
 
 describe('Github API', () => {
 
@@ -7,3 +8,72 @@ describe('Github API', () => {
     })
 
 })
+
+test('End-to-End Testing --> Repo with No Open Pull Requests', async () => {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--window-size=1920,1080']
+    });
+
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8080/');
+    await page.click('input#search-repo');
+    await page.type('input#search-repo', 'jparrott06/test');
+    await page.click('button');
+
+    await page.waitForSelector('p');
+
+    const noPullsText = await page.$eval('p.no-pulls', el => el.textContent);
+
+    expect(noPullsText).toBe('No Open Pull Requests for this Repo');
+
+    browser.close();
+
+}, 120000);
+
+test('End-to-End Testing --> Repo that does not exist', async () => {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--window-size=1920,1080']
+    });
+
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8080/');
+    await page.click('input#search-repo');
+    await page.type('input#search-repo', 'NotaUserjdiofjsj/repoNooo');
+    await page.click('button');
+
+    await page.waitForSelector('p');
+
+    const errorsText = await page.$eval('p.error', el => el.textContent);
+
+    expect(errorsText).toBe('Error: Request failed with status code 404');
+
+    browser.close();
+
+}, 120000);
+
+test('End-to-End Testing --> Repo with Open Pull Requests', async () => {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--window-size=1920,1080']
+    });
+
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8080/');
+    await page.click('input#search-repo');
+    await page.type('input#search-repo', 'jparrott06/ChirpyApp');
+    await page.click('button');
+
+    await page.waitForSelector('p.total_commits');
+
+    const errorsText = await page.$eval('p.total_commits', el => el.textContent);
+
+    expect(errorsText).toBe('Total Commits: 107');
+
+    browser.close();
+
+}, 120000);
