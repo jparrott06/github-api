0eb8c3a7e8a1838bc82b2623d0dc1fb5b9948a08
jparrott06 <48459550+jparrott06@users.noreply.github.com>
2022-01-26 18:09:23 -0700

    Updated unit/integration testing for apiController.js with more specific info
    - Updated tests to test for specific error messages
    - Updated tests to test for specific status codes

--- server/controllers/apiController.test.js
+++ server/controllers/apiController.test.js
@@ -1,5 +1,7 @@
 const apiController = require('./apiController');
 
+const Pulls = require('../helpers/Pulls');
+
 // Unit & Integration Testing for apiController
 
 describe('Test apiController ', () => {
@@ -27,9 +29,21 @@ describe('Test apiController ', () => {
     try {
       await apiController.getOpenPRs(mReq, mResp, mNext)
     } catch(err) {
-      //Error: Request failed with status code 404
-      expect(err);
+      expect(err.status).toBe(404);
+      expect(err.message).toBe('Request failed with status code 404');
     }
+
+  }),
+
+  test('getOpenPRs - should return 200 for repo with open PRs', async () => {
+
+    const mReq = { query: {text: 'jparrott06/ChirpyApp'} };
+    const mRes = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+    const mNext = jest.fn();
+
+    await apiController.getOpenPRs(mReq, mRes, mNext);
+
+    expect(mRes.send).toBeCalled;
 
   }),
 
@@ -42,42 +56,23 @@ describe('Test apiController ', () => {
     try {
       await apiController.getCommitNum(mReq, mResp, mNext)
     } catch(err) {
-      //Error: 
-      expect(err);
+      expect(err.message).toBe("Sorry, something went wrong with your application :'(");
     }
 
   }),
 
   test('getCommitNum - No Open PRs in prArray', async () => {
 
-    const mReq = { query: {text: 'dfoisfuosfdns/dfijdifsni'} };
-    const mResp = {locals: {prArray: [{}]}};
+    const mReq = { query: {text: 'jparrott06/test'} };
+    const mResp = {locals: {prArray: []}};
     const mNext = jest.fn();
 
     try {
       await apiController.getCommitNum(mReq, mResp, mNext)
     } catch(err) {
-      //Error: 
-      expect(err);
+      expect(err.message).toBe("Sorry, something went wrong with your application :'(");
     }
 
   })
-
-  /*
-  TODO: Test successful cases for apiController functions
-  for unit/integration testing
-
-  Warning: tests using HTTP requests to api.github
-  can return false negative since repeated GET calls testing triggers rate limit
-
-  api/github.com/rate_limit
-
-  res.locals = {prArray: [{
-    number: 1,
-    state: 'open',
-    compareUrl: etc.
-  }]}
-
-  */
 
 });
